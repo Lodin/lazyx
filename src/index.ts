209a211b955b006db49c05a37e5bd80abc340ea6
Vlad Rindevich <rindevich.vs@gmail.com>
2017-05-22 20:30:49 +0300

    build: initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export class Student {
+  public fullName: string;
+
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string,
+  ) {
+    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+  }
+}
+
+export interface IPerson {
+  firstName: string;
+  lastName: string;
+}
+
+export function greeter(person: IPerson): string {
+  return `Hello, ${person.firstName} ${person.lastName}`;
+}
