ff3f005f6693a2d2e4f216b626692916647e05c8
Vlad Rindevich <rindevich.vs@gmail.com>
2017-05-31 20:31:17 +0300

    feat(core): add Store and Middleware types export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './add/operator/apply';
 import './add/operator/mergeSequence';
 
+export {Store} from './createStore';
 export {default as createStore} from './createStore';
 export {SubjectMap} from './SubjectMap';
+export {Middleware} from './typings';
