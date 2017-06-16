7e4ce3d8adc940536f2ad0a4fbed451a7e5452d9
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-16 16:47:19 +0300

    build(config): replace "async/await" to Promises in "make-packages" script to provide compatibility for node 6

--- config/make-packages.js
+++ config/make-packages.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const promisify = require('util.promisify');
+const pkg = require('../package.json');
 
 const absolutify = rootPath => path.resolve(__dirname, '..', rootPath);
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-(async function makePackages(pkg) {
-  const cjsPkg = Object.assign({}, pkg, {
-    main: 'index.js',
-    types: 'index.d.ts',
-  });
+const cjsPkg = Object.assign({}, pkg, {
+  main: 'index.js',
+  types: 'index.d.ts',
+});
 
-  delete cjsPkg.scripts;
+delete cjsPkg.scripts;
 
-  writeFile(absolutify('dist/cjs/package.json'), JSON.stringify(cjsPkg, null, 2));
-  writeFile(absolutify('dist/cjs/LICENSE'), await readFile('LICENSE'));
-  writeFile(absolutify('dist/cjs/README.md'), await readFile('README.md'));
-}(require('../package.json')));
+writeFile(absolutify('dist/cjs/package.json'), JSON.stringify(cjsPkg, null, 2));
+readFile('LICENSE').then(data => writeFile(absolutify('dist/cjs/LICENSE'), data));
+readFile('README.md').then(data => writeFile(absolutify('dist/cjs/README.md'), data));
