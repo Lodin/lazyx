3044e172ef118fdaac196816dd80cd1670b8da91
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-17 20:51:08 +0300

    fix(core): simplify export from "createStore" module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import './add/operator/apply';
 import './add/operator/mergeCollection';
 
-export {Store} from './createStore';
-export {default as createStore} from './createStore';
+export {default as createStore, Store} from './createStore';
 export {SubjectMap} from './SubjectMap';
 export {Middleware} from './typings';
